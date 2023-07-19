9be501c2e3272cef7fce3b204d858f4974fa16ea
Aj-1872 <ajayjodedra@gmail.com>
2023-07-19 08:11:52 -0700

    done with the design of sign in page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sing_in from './Component/Sing_in';
+import Home from './Component/Home';
 
 function App() {
   return (
-    <div >
-  <Sing_in/>
-    </div>
+    <>
+      <Router>
+        <div >
+          <Routes>
+            <Route path='/' element={<Sing_in />}></Route>
+            <Route path='/Home' element={<Home />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
--- src/Component/Sing_in.js
+++ src/Component/Sing_in.js
@@ -1,94 +1,90 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-
+// Rest of your code...
 
 
 export class Sing_in extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.userRef = React.createRef();
-        this.passRef = React.createRef();
-    }
-
-    userNameSingIn = () => {
-
-    }
-    passSingIn = () => {
-
-    }
+    this.userRef = React.createRef();
+    this.passRef = React.createRef();
+  }
 
 
 
+  render() {
+    return (
+      <div className="flex justify-center items-center " id='main' >
+        <div className="w-90">
+          <form >
+            <h1 className="text-4xl font-bold mb-8 font-billabong text-center mt-7">Instagram</h1>
 
-
-    render() {
-        return (
-            <div>
-
-                <div className="flex justify-center items-center h-screen">
-                    <div className="w-96">
-                        <h1 className="text-4xl font-bold mb-8 font-billabong ">Instagram</h1>
-                        <form>
-                            <div className="mb-4">
-                                <input ref={this.userRef}
-                                    onClick={() => { this.userNameSingIn() }}
-
-                                    type="text"
-                                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none"
-                                    placeholder="Username"
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <input ref={this.passRef}
-                                    onClick={() => { this.passSingIn() }}
-                                    type="password"
-                                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none"
-                                    placeholder="Password"
-                                />
-                            </div>
-                            <button
-                                className="w-full py-2 bg-blue-600 text-white rounded-md"
-                            >
-                                log in
-                            </button>
-                            <div className="mt-4 text-center appearance-none list-none">
-
-                                <a href="/" className="text-black-500 hover:underline ">
-                                    Forgot your login details? <span className='font-medium'>Get help singin in.</span>
-                                </a>
-                            </div>
-                            <div class="flex justify-center mt-28">
-                                <div class="w-2/3 flex items-center">
-                                    <div class="flex-grow border-b-2 border-gray-300"></div>
-                                    <div class="mx-4 text-gray-500">or</div>
-                                    <div class="flex-grow border-b-2 border-gray-300"></div>
-                                </div>
-                            </div>
-
-                            <div className="mt-0 text-center appearance-none list-none">
-
-                                <a href="#" className="fa fa-facebook text-white bg-black w-6 h-4.5 m-1 text-xl rounded "></a>
-                                <a href="/" className="text-black-500 hover:underline ">
-                                    <span className='font-medium'>Log in with Facebook</span>
-                                </a>
-                            </div>
-                            <div className="mt-10 text-center appearance-none list-none">
-                            <hr/>
-                            <a href="/" className="text-black-500 hover:underline ">
-                                Don't have an account?<span className='font-medium'>Sing Up.</span>
-                            </a>
-                        </div>
-
-                        </form>
-                    </div>
-                </div>
-
-
-
+            <div className="flex items-center justify-center py-2 bg-blue-500 text-white rounded-md mt-14">
+              <a href="#" className="text-blue-500 bg-white w-5 h-5 m-1 text-xl fa fa-facebook flex justify-end"></a>
+              <a href="/" className="text-white hover:underline">
+                <span className="font-medium">Log in with Facebook</span>
+              </a>
             </div>
-        )
-    }
+
+            <div className="flex justify-center my-3">
+              <div className="w-2/3 flex items-center">
+                <div className="flex-grow border-b-2 border-gray-300"></div>
+                <div className="mx-4 text-gray-500">OR</div>
+                <div className="flex-grow border-b-2 border-gray-300"></div>
+              </div>
+            </div>
+
+            <div className="mb-2">
+              <input
+                ref={this.userRef}
+                type="text"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none"
+                placeholder="Username"
+              />
+            </div>
+
+            <div className="mb-4">
+              <input
+                ref={this.passRef}
+                type="password"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none"
+                placeholder="Password"
+              />
+            </div>
+
+            <div className="mt-2 mb-6 text-center appearance-none list-none">
+              <a href="/" className="text-black-500 hover:underline">
+                Forgot your login details? <span className="font-medium">Get help signing in.</span>
+              </a>
+            </div>
+
+            <div className="flex items-center justify-center py-2 bg-blue-500 text-white rounded-md ">
+            <Link to="/Home" className=" text-center w-full py-2 bg-blue-500 text-white rounded-md">Log in</Link>
+          </div>
+          
+
+            <div className="mt-8 text-center appearance-none list-none">
+              <a href="/" className="text-black-500 hover:underline">
+                Don't have an account?<span className="font-medium text-blue-600"> Sign up.</span>
+              </a>
+            </div>
+
+            
+            <div className='margin'>
+            <div className='text-center '><p className='text-sm text-gray-500'>from</p></div>
+            <div className="flex items-center justify-center ">
+            <img src={require('./image/6033716.png')} alt="" className="h-5 w-5" />
+            <span className="text-blue-600 mx-1 font-medium">Meta</span>
+            </div>
+            </div>
+            
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Sing_in
+export default Sing_in;
